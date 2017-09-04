45dc7d48b548d6907ff15cfd0919f32f6aa67cbd
Seth J. Connell <sethjordanconnell@gmail.com>
2017-09-04 12:42:22 -0500

    Organized Response

--- LookUpPoliticians.php
+++ LookUpPoliticians.php
@@ -5,16 +5,38 @@ require_once 'vendor/autoload.php';
 function search($address) {
     $client = new Google_Client();
     $client->setApplicationName("Client_Library_Examples");
-    $client->setDeveloperKey("xxxxxxxxxxxxxxxxxxxxx"); // Replace Xs with API Key Generated for Google Civics API.
+    $client->setDeveloperKey("xxxxxxxxxxxxxxxx"); // Replace Xs with API Key Generated for Google Civics API.
     
     $civicinfoService = new Google_Service_CivicInfo($client);
     $representatives = $civicinfoService->representatives->representativeInfoByAddress(
         array("address" => "$address"),
-        array("includeOffices" => false));
+        array("includeOffices" => true),
+        array("roles" => "legislatorLowerBody")
+    );
         
     foreach ($representatives as $item) {
-        echo "\n", $item['name'];
+        echo "\n", $item["name"];
+        foreach ($item["address"] as $line) {
+            if ($line['line1'] != ""){
+                echo "\n", $line['line1'];
+            }
+            if ($line['line2'] != '') {
+                echo "\n", $line['line2'];
+            }
+            if ($line['city'] != '') {
+                echo "\n", $line['city'];
+            }
+            if ($line['state'] != '') {
+                echo "\n", $line['state'];
+            }
+            if ($line['zip'] != '') {
+                echo "\n", $line['zip'];
+            }
+            echo "\n";
+            echo "\n";
+        }
     }
 }
 
+search("103 Blue Ridge Court, Fairfield Bay, Arkansas"); // Example of usage
 ?>
